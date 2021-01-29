592ff0d8b15a0bdb8ea97320410610f5ce94b142
RNorris6 <ryan.norris@patelhs.org>
2021-01-29 23:30:53 +0000

    final changes fixed issue, had to retype part of methods.

--- Main.java
+++ Main.java
@@ -1,60 +1,59 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args)
   {
-    //created scanners and the original print statements
-    Scanner scan = new Scanner(System.in);
-    System.out.println("What should be your A value");
-    int aValue = scan.nextint();
-    System.out.println("What should be your B value");
-    int bValue = scan.nextint();
-
-    System.out.println("This is your result for your work");
-
-    //math part of the code
-    int sum = addition(aValue, bValue);
-    System.out.println("If you add," + aValue + "and" + bValue + "you would get" + sum);
-
-    int diff = subtraction (aValue, bValue);
-    System.out.println("If you subtract the values," + aValue + "and" + bValue + "you would get" + diff);
-
-    int product = multiplication (aValue, bValue);
-    System.out.println("If you multiply the values," + aValue + "and" + bValue + "you would get" + product);
-
-    int quotient = division (aValue, bValue);
-    System.out.println("If you divide the values," + aValue + "and" + bValue + "you would get" + quotient);
+   Scanner scan= new Scanner(System.in);
+   System.out.println("What should be your A value?");
+   int aValue = scan.nextInt();
+   System.out.println("What Should be your B value");
+   int bValue = scan.nextInt();
 
 
+   System.out.println("Here's your math!");
+
+   int sum = addition  (aValue, bValue);
+   System.out.println("If you add, " + aValue + " and " + bValue + " and " + " you get " + sum);
+
+   int diff = subtraction(aValue, bValue);
+   System.out.println("If you subtract, " + aValue + " and " + bValue + " and you get " + diff);
+
+   int product = multiplication (aValue, bValue);
+   System.out.println("If you multiply, " + aValue + " and " + bValue + " and you get " + product);
+
+   int quotient = division (aValue,bValue);
+   System.out.println("If you divide, " + aValue + " by " + bValue + " you get " + quotient);
+ 
+   
   }
     
-    //addition method 
-    static int addition(int aValue, int bValue)
-    {
-      int sum = aValue + bValue;
-      return sum;
-    }
-
-    //subtraction method
-    static int subtraction(int aValue, int bValue)
-    {
-      int diff = aValue - bValue;
-      return diff;
-    }
-
-    //multiplication method
-    static int multiplication(int aValue, int bValue)
-    {
-      int product = aValue * bValue;
-      return product;
-    }
-
-    //division method
-    static int division(int aValue, int bValue)
-    {
-      int quotient = aValue / bValue;
-      return quotient;
-     }
     
+    // addition method for math
+  static int addition(int aValue, int bValue)
+  {
+    int sum = aValue + bValue;
+    return sum;
+  }
+
+  // subtraction method for math
+  static int subtraction(int aValue, int bValue)
+  {
+    int diff = aValue - bValue;
+    return diff;
+  }
+ 
+   // multiplication method for math
+   static int multiplication(int aValue, int bValue)
+   {
+    int product = aValue * bValue;
+    return product;
+   }
+
+    // division method for math
+   static int division(int aValue, int bValue)
+   {
+    int quotient = aValue / bValue;
+    return quotient;
+   }
   
 }
